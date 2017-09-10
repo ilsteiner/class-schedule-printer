c8c86c3cf54b2e5e0fd231063e607e4bce94623f
Isaac Steiner <ilebwohlsteiner@gmail.com>
2017-09-10 14:02:37 -0400

    Fix animation stuff

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var sasslint = require('gulp-sass-lint');
 var minify = require('gulp-clean-css');
 var prefix = require('gulp-autoprefixer');
 var merge = require('gulp-merge-json');
@@ -29,6 +31,11 @@ gulp.task('json-merge', ['json-trim'], function() {
 // Compile Our Sass
 gulp.task('compile-sass', function() {
     return gulp.src('scss/*.scss')
+        /*.pipe(sasslint({
+            configFile: 'config/.sass-lint.yml'
+         }))
+        .pipe(sasslint.format())
+        .pipe(sasslint.failOnError())*/
         .pipe(sass())
         .pipe(prefix())
         .pipe(gulp.dest('css'));
@@ -38,6 +45,8 @@ gulp.task('compile-sass', function() {
 gulp.task('js', function() {
     gulp.src('js/*.js')
     .pipe(concat('concat.js'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
     .pipe(uglify())
     .on('error', function(err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
     .pipe(gulp.dest('dist/js/'));
