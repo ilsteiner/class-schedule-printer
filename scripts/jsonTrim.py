024e7b0168dc518b99d8ebce2aaac2ae7339609e
Isaac <ilebwohlsteiner@gmail.com>
2017-12-15 20:58:07 -0500

    New data

--- scripts/jsonTrim.py
+++ scripts/jsonTrim.py
@@ -3,20 +3,30 @@ import os
 import json
 from datetime import datetime
 import time
+import logging
 
 directory = 'data/json/'
+logging.basicConfig(filename='trim.log',level=logging.DEBUG)
 
 # Clean the trimmed directory first
 for filename in glob.glob(directory + 'trimmed/*.json'):
 	os.remove(filename)
 
+logging.info("Cleared directory")
+
+file_count = 0
+
 for filename in glob.glob(directory + '*.json'):
+	file_count += 1
+
 	with open(filename) as jsonFile:
 		# Get data
 		data = json.load(jsonFile)
 
 		# Get just the attendee array
 		attendees = data["Attendees"]
+
+		logging.info("There are " + str(len(attendees)) + " attendees.")
 		
 		# Make a date object
 		date = datetime.strptime(
@@ -34,3 +44,4 @@ for filename in glob.glob(directory + '*.json'):
 		# Write just the attendees to a new file
 		with open(directory + "trimmed/" + os.path.basename(filename),'w') as output:
 			json.dump(sorted_attendees,output,indent=4, sort_keys=True)
+logging.info("Processed " + str(file_count) + " files")
