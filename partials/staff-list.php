9adcaed4846992c818fb20a9952918bd0f33457e
Isaac <ilebwohlsteiner@gmail.com>
2017-12-20 01:10:14 -0500

    blah

--- partials/staff-list.php
+++ partials/staff-list.php
@@ -3,12 +3,12 @@
 	foreach ($lists["staff_lists"] as $list) {
 		echo "<div class='staff-list'>";
 			echo "<div class='staff-list-header'>";
-				echo "<div class='class-name'>" . $list->"name" . "</div>";
-				echo "<div class='leader-name'>" . $list->"leader" . "</div>";
+				echo "<div class='class-name'>" . $list->name . "</div>";
+				echo "<div class='leader-name'>" . $list->leader . "</div>";
 				echo "<div class='count" . (atLimit($className) ? " limit" : "") . "'>" . count($list["participants"]) . "</div>";
 			echo "</div>";
 			echo "<div class='participants'>";
-				foreach ($list->"participants" as $participant => $pData) {
+				foreach ($list->participants as $participant => $pData) {
 					echo "<div class='participant'>";
 						echo "<div class='participant-name'>" . $participant . "</div>";
 					echo "</div>";
