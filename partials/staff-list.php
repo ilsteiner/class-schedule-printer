b19ace9f4bb8112d61f05a58edb63c31324bff97
Isaac <ilebwohlsteiner@gmail.com>
2017-12-20 00:55:03 -0500

    update staff lists

--- partials/staff-list.php
+++ partials/staff-list.php
@@ -16,25 +16,16 @@
 <div class="staff-list-wrapper">
 	<?php
 	foreach ($lists["staff_lists"] as $list) {
-		print_r($list);
 		echo "<div class='staff-list'>";
 			echo "<div class='staff-list-header'>";
-				echo "<div class='class-name'>" . $nameMap[$className] . "</div>";
-				echo "<div class='leader-name'>" . $data["leader"] . "</div>";
-				echo "<div class='count" . (atLimit($className) ? " limit" : "") . "'>" . count($data["participants"]) . "</div>";
+				echo "<div class='class-name'>" . $list["name"] . "</div>";
+				echo "<div class='leader-name'>" . $list["leader"] . "</div>";
+				echo "<div class='count" . (atLimit($className) ? " limit" : "") . "'>" . count($list["participants"]) . "</div>";
 			echo "</div>";
 			echo "<div class='participants'>";
-				foreach ($data["participants"] as $participant => $pData) {
-					$time = $pData["time"];
+				foreach ($list["participants"] as $participant => $pData) {
 					echo "<div class='participant'>";
-						echo "<div class='participant-name'>" . $participant . (isset($pData["age"]) ? "<span class='age'>" . $pData["age"] . "</span>" : "") . "</div>";
-						echo "<div class='registration-time' title='Timezone:  " .
-								date("T, P", $time) .
-								"'>" .
-								"<span class='reg-date'>" . date("m/d/y", $time) . "</span>" .
-								"<span class='reg-time'>" . date("g:i", $time) . "</span>" .
-								"<span class='reg-ampm'>" . date("A", $time) . "</span>" .
-							"</div>";
+						echo "<div class='participant-name'>" . $participant . "</div>";
 					echo "</div>";
 				}
 			echo "</div>";
